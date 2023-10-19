3411f1d63c532198bcdb635201026a4e6f13aaf4
Romário Lima <devromariolima@gmail.com>
2023-10-19 15:22:08 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Calculator from './components/Calculator'
+import './components/Calculator.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Calculadora</h1>
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Calculator.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Container from '@mui/material/Container';
+import { Box } from "@mui/material";
+import './Calculator.css';
+
+export default function Calculator() {
+
+const [num , setNum]= useState(0);
+
+function inputNum (e){
+    var input = e.target.value;
+    if(num === 0){
+    setNum(input);
+ }else {
+    setNum(num + input);
+ }
+    
+};
+
+function clear (){
+    setNum(0);
+}
+
+function porcentage () {
+    setNum(parseFloat(num) /  100)
+    console.log("porcentage", num);
+    console.log("porcentage",typeof num);
+}
+
+function changeSign () {
+   if (num > 0 ){
+    setNum(-num);
+   } else{
+    setNum(Math.abs(num));
+   }
+
+}
+
+  return (
+
+<div>
+ <Box m={2}/>
+<Container maxWidth="xs">
+    <div className="wrapper">
+    <Box m={12}/>
+     <h1 className="result">{num}</h1>
+     <button className="gray" onClick={inputNum}value={1}>1</button>
+     <button className="gray" onClick={inputNum}value={2}>2</button>
+     <button className="gray" onClick={inputNum}value={3}>3</button>
+     <button className="orange" onClick={porcentage}>%</button>
+     <button className="gray" onClick={inputNum}value={4}>4</button>
+     <button className="gray" onClick={inputNum}value={5}>5</button>
+     <button className="gray" onClick={inputNum}value={6}>6</button>
+     <button className="orange">+</button>
+     <button className="gray" onClick={inputNum}value={7}>7</button>
+     <button className="gray" onClick={inputNum}value={8}>8</button>
+     <button className="gray" onClick={inputNum}value={9}>9</button>
+     <button className="orange">-</button>
+     <button className="gray" onClick={inputNum}value={0}>0</button>
+     <button className="gray" onClick={inputNum}value={","}>,</button>
+     <button className="orange" onClick={clear}>AC</button>
+     <button className="orange">/</button>
+     <button className="orange" onClick={changeSign}>+-</button>
+     <button className="orange">=</button>
+    </div>
+    </Container>
+    </div>
+  );
+  }
