84d00d140bf68436aee65b03eb6fc607601d51a6
Romário Lima <devromariolima@gmail.com>
2023-10-19 15:39:10 -0300

    ajustes na ordenação dos botões

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -36,6 +36,10 @@ function changeSign () {
 
 }
 
+function calculate(){
+  console.log("calculou")
+ }
+
   return (
 
 <div>
@@ -44,10 +48,14 @@ function changeSign () {
     <div className="wrapper">
     <Box m={12}/>
      <h1 className="result">{num}</h1>
+     <button className="white" onClick={clear}>AC</button>
+     <button className="white" onClick={changeSign}>+-</button>
+     <button className="white" onClick={porcentage}>%</button>
+     <button className="orange">/</button>
      <button className="gray" onClick={inputNum}value={1}>1</button>
      <button className="gray" onClick={inputNum}value={2}>2</button>
      <button className="gray" onClick={inputNum}value={3}>3</button>
-     <button className="orange" onClick={porcentage}>%</button>
+     <button className="orange">X</button>
      <button className="gray" onClick={inputNum}value={4}>4</button>
      <button className="gray" onClick={inputNum}value={5}>5</button>
      <button className="gray" onClick={inputNum}value={6}>6</button>
@@ -58,10 +66,7 @@ function changeSign () {
      <button className="orange">-</button>
      <button className="gray" onClick={inputNum}value={0}>0</button>
      <button className="gray" onClick={inputNum}value={","}>,</button>
-     <button className="orange" onClick={clear}>AC</button>
-     <button className="orange">/</button>
-     <button className="orange" onClick={changeSign}>+-</button>
-     <button className="orange">=</button>
+     <button className="orange" onClick={calculate}>=</button>
     </div>
     </Container>
     </div>
